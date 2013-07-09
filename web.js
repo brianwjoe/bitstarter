00b5fda3fee6e5b1d7123481e16e79af6c9f8c9d
Ubuntu <ubuntu@ip-172-31-27-4.us-west-2.compute.internal>
2013-07-09 17:22:06 +0000

    bug fixes to web.js

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require ('fs');
 var app = express.createServer(express.logger());
-var inputfile = "index.html"
+var inputfile = "index.html";
 
 
 app.get('/', function(request, response) {
-  
+
   fs.readFile(inputfile, function (err, data) {
       if (err) throw err;
       reponse.send(data);
-  }
+  });
   // response.send("Hello World 4");
 });
 
