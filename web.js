2b2d0315fa96b6ca9503145d8cdcc72dfdf9d5dd
Ubuntu <ubuntu@ip-172-31-27-4.us-west-2.compute.internal>
2013-07-09 18:52:50 +0000

    bug fixes to web.js - enabled file reading from index.html

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var fs = require ('fs');
+// var buffer = require ('buffer');
 var app = express.createServer(express.logger());
-var inputfile = "index.html";
+var inputfile = "./index.html";
 
 
 app.get('/', function(request, response) {
 
   fs.readFile(inputfile, function (err, data) {
       if (err) throw err;
-      reponse.send(data);
+      data = data.toString();
+      response.send(data);
   });
   // response.send("Hello World 4");
 });
